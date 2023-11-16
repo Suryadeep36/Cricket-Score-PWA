fa0b2cbf76f4622c7985b395e801c773caebb43a
Suryadeep36 <75900590+Suryadeep36@users.noreply.github.com>
2023-11-16 19:22:43 +0530

    Update worker.js

--- client/worker.js
+++ client/worker.js
@@ -4,15 +4,13 @@ self.addEventListener("push", e => {
     if(data.bowTeam == " "){
         data.bowTeam = "Another team yet to bat";
     }
-    e.waitUntil(
         self.registration.showNotification(
-            data.title, // title of the notification
-            {
-                body: data.batTeam + "\n" + data.bowTeam + "\n" + data.condition, //the body of the push notification
-                image: "https://pixabay.com/vectors/bell-notification-communication-1096280/",
-                icon: "https://pixabay.com/vectors/bell-notification-communication-1096280/" // icon 
-            }
-        )
-    );
+        data.title, // title of the notification
+        {
+            body: data.batTeam + "\n" + data.bowTeam + "\n" + data.condition, //the body of the push notification
+            image: "https://pixabay.com/vectors/bell-notification-communication-1096280/",
+            icon: "https://pixabay.com/vectors/bell-notification-communication-1096280/" // icon 
+        }
+    )
 });
 
